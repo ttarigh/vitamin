dda48349b1d64b8f58ffce205512463ee602e733
ttarigh <99256332+ttarigh@users.noreply.github.com>
2024-04-28 15:40:57 -0400

    first draft

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-const form = document.getElementById('audioForm');
 
-form.addEventListener('submit', (event) => {
+const form = document.getElementById("audioForm");
+const sub = document.getElementById("submitButton");
+const resetButton = document.getElementById("resetButton");
+
+form.addEventListener("submit", (event) => {
   event.preventDefault(); // Prevent default form submission
+  sub.style.display = "none";
+  document.body.style.backgroundColor = "blue";
+  document.body.style.color = "blue";
 
   const checkedBoxes = form.querySelectorAll('input[type="checkbox"]:checked');
 
-  checkedBoxes.forEach(checkbox => {
+  const allCheckboxes = form.querySelectorAll('input[type="checkbox"]');
+      allCheckboxes.forEach((box) => {
+        if (!box.checked) {
+          box.style.display = "none";
+        }
+      });
+  checkedBoxes.forEach((checkbox) => {
     const audio = new Audio(checkbox.value);
+    const label = checkbox.parentElement; // Get the label element
+    label.style.backgroundColor = "yellow";  
+    // label.style.color = "black";  
     audio.play();
+
+    // Stop audio after 1 minute (60 seconds)
+    setTimeout(() => {
+      sub.style.display = "block";
+      
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      
+      label.style.backgroundColor = "transparent";  
+     
+      const allCheckboxes = form.querySelectorAll('input[type="checkbox"]');
+      allCheckboxes.forEach((box) => {
+        if (!box.checked) {
+          box.style.display = "inline-flex";
+        }
+      });
+      audio.pause();
+      checkbox.checked = false;
+      resetButton.click();
+    }, 6 * 1000); // 60 seconds * 1000 milliseconds
   });
 });
+
